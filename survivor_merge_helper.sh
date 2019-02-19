d58e3476e577e11967d969201adbf2a77b2c3b66
apaul7 <alex.paul@wustl.edu>
2019-02-19 14:29:46 -0600

    added nounset error and fixed uncompress method

--- survivor_merge_helper.sh
+++ survivor_merge_helper.sh
@@ -2,14 +2,18 @@
 
 set -o pipefail
 set -o errexit
+set -o nounset
 
 # uncompress files as needed
-for i in $(echo ${3} | tr "," "\n"); do
+for i in $(echo ${1} | tr "," "\n"); do
     if [[ $i == *.gz ]]; then
-        gunzip "$i"
-        i=$(echo "$i" | sed 's/\(.*\)\.gz/\1/')
+        cp "$i" .
+        name=$(basename "$i")
+        gunzip "$name"
+        name=$(echo "$name" | sed 's/\(.*\)\.gz/\1/')
+        i="$(pwd)/$name"
     fi
     echo "$i" >> vcfs.list
 done
 
-echo "/usr/bin/SURVIVOR merge vcfs.list ${4} ${5} ${6} ${7} ${8} ${9} ${10}"
+/usr/bin/SURVIVOR merge vcfs.list ${2} ${3} ${4} ${5} ${6} ${7} ${8}
